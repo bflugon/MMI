8245b9ef9ba1e125c905f1a05d058abaaada00cb
Båd <bflugon@gmail.com>
2014-02-04 12:19:14 +0100

    Starter T3

---
+++ src/T3/Person.java
@@ -0,0 +1,53 @@
+package T3;
+
+
+public class Person {
+    private String name;
+    private String dateOfBirth;
+    private String email;
+    private float height;
+    public Gender gender;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+
+}
---
+++ src/T3/PersonPanelController.java
@@ -0,0 +1,42 @@
+package T3;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+
+public class PersonPanelController {
+    public TextField name;
+    public TextField email;
+    public TextField dateOfBirth;
+    public ComboBox<Gender> gender;
+    public Slider height;
+
+    private Person model;
+
+
+    public void update(){
+        if (model != null) {
+        model.setName(name.getText());
+        model.setDateOfBirth(dateOfBirth.getText());
+        model.setEmail(email.getText());
+        model.setHeight((float)height.getValue());
+        model.gender = gender.getValue();
+            System.out.println("Model updated");
+        }
+    }
+
+    public Person getModel() {
+        return model;
+    }
+
+    public void setModel(Person person) {
+        this.model = person;
+        name.setText(person.getName());
+        email.setText(person.getEmail());
+        dateOfBirth.setText(person.getDateOfBirth());
+        height.setValue(person.getHeight());
+        gender.setValue(model.getGender());
+
+    }
+}
